524366d773c29198380c50f9a14002ed4409046e
kelaskakap <rj.fikri@kelaskakap.com>
2018-02-18 07:10:24 +0700

    add more ad size

--- lazy.adsense.js
+++ lazy.adsense.js
@@ -7,5 +7,17 @@ jQuery(document).ready(function ($) {
             $this.addClass('adsense--loaded');
         }
     });
+	
+	$('.adsense728x90').adsenseLoader({
+        onLoad: function ($this) {
+            $this.addClass('adsense--loaded');
+        }
+    });
+	
+	$('.adsense300x600').adsenseLoader({
+        onLoad: function ($this) {
+            $this.addClass('adsense--loaded');
+        }
+    });
 
 });
